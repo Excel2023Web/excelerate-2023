9401cdf2812f97faebb8efe4b3f4515e7baedc50
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-09-10 20:48:12 +0530

    packages

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
 const MainPage = () => {
   return (
     <div>
-      <h1>Excelerate website</h1>
+      <Navbar/>
     </div>
   )
 }
