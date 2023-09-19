f4965daed945bbc3c4aa9f945ec22ce41a26341e
jacobsanosh <jacobsanosh@gmail.com>
2023-09-19 12:14:40 +0530

    resposiblity and benefits

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Landing.css";
 import landing from "../../assets/landing.svg";
+import CountUp from "react-countup";
 function Landing() {
   return (
     <div className="landing__container">
@@ -24,7 +25,14 @@ function Landing() {
       <div className="landing__features">
         <div className="landing__card">
             <div className="landing__card__heading">
-                <h1>40k</h1>
+                <h1>
+                <CountUp
+                end={40}
+                duration={2}
+                enableScrollSpy={true}
+                preserveValue={true}
+              />
+                  k</h1>
             </div>
             <div className="landing__card__content">
                 <h4>FootFall</h4>
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Navbar,Landing,Contacts, Footer } from '../../components'
+import { Navbar,Landing,Contacts, Footer, RespAndBenefits } from '../../components'
 const MainPage = () => {
   return (
     <div className='main__container'>
       <Navbar/>
       <Landing/>
+      <RespAndBenefits/>
       <Contacts/>
       <Footer/>
     </div>
