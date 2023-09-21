14d8d88c277940c3e87ff3612bbdce28f7572fbb
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-09-21 22:52:57 +0530

    landing section made responsive

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,56 +6,47 @@ function Landing() {
   return (
     <div className="landing__container">
       <div className="landing__container_up">
-        <div className="landing__left__section">
-          <img src={landing} alt="" />
-        </div>
+        <img className="landing__left__section" src={landing} alt="" />
         <div className="landing__right__section">
-          <div className="landing__right__top">
-            <h1>
-              Campus
-              <br />
-              Ambassador
-            </h1>
-          </div>
-          <div className="landing__right__bottom">
-            <button className="landing__reg__button">Register</button>
-          </div>
+          <h1>Campus Ambassador</h1>
+          <button className="reg__btn">Register</button>
         </div>
       </div>
+
       <div className="landing__features">
         <div className="landing__card">
-            <div className="landing__card__heading">
-                <h1>
-                <CountUp
+          <div className="landing__card__heading">
+            <h1>
+              <CountUp
                 end={40}
                 duration={2}
                 enableScrollSpy={true}
                 preserveValue={true}
               />
-                  k</h1>
-            </div>
-            <div className="landing__card__content">
-                <h4>FootFall</h4>
-            </div>
+              k
+            </h1>
+          </div>
+          <div className="landing__card__content">
+            <h4>FootFall</h4>
+          </div>
         </div>
         <div className="landing__card">
-            <div className="landing__card__heading">
-                <h1>40+</h1>
-            </div>
-            <div className="landing__card__content">
-                <h4>Events</h4>
-            </div>
+          <div className="landing__card__heading">
+            <h1>40+</h1>
+          </div>
+          <div className="landing__card__content">
+            <h4>Events</h4>
+          </div>
         </div>
         <div className="landing__card">
-            <div className="landing__card__heading">
-                <h1>40k</h1>
-            </div>
-            <div className="landing__card__content">
-                <h4>Prize Pool</h4>
-            </div>
+          <div className="landing__card__heading">
+            <h1>40k</h1>
+          </div>
+          <div className="landing__card__content">
+            <h4>Prize Pool</h4>
+          </div>
         </div>
       </div>
-        
     </div>
   );
 }
