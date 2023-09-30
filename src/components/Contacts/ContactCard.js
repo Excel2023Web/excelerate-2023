bae92abf4c91f94c19a15d3bcf9fcf0012fa778b
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-09-30 12:36:53 +0530

    ctacts

--- src/components/Contacts/ContactCard.js
+++ src/components/Contacts/ContactCard.js
@@ -3,17 +3,13 @@ import "./Contacts.css";
 import { FiLinkedin, FiMail, FiPhoneCall } from "react-icons/fi";
 import Aos from "aos";
 
-const ContactCard = ({contact}) => {
+const ContactCard = ({ contact }) => {
   useEffect(() => {
     Aos.init({ duration: 1100 });
   }, []);
   return (
     <div data-aos="zoom-in" aos-delay="200" className="contact__card_main">
-      <img
-        src={contact.image}
-        className="ctct_image"
-        alt=""
-      />
+      <img src={contact.image} className="ctct_image" alt="" />
       <div className="contact__card_desc_part">
         <div className="name_n_pos_ctct">
           <p className="ctct__name">{contact.name}</p>
@@ -21,9 +17,15 @@ const ContactCard = ({contact}) => {
         </div>
 
         <div className="ctct__icons">
-          <FiLinkedin className="social__icon_contact" />
-          <FiMail className="social__icon_contact" />
-          <FiPhoneCall className="social__icon_contact" />
+          <a href={contact.linkedIn} target="_blank" rel="noreferrer">
+            <FiLinkedin className="social__icon_contact" />
+          </a>
+          <a href={`mailto:${contact.email}`} target="_blank" rel="noreferrer">
+            <FiMail className="social__icon_contact" />
+          </a>
+          <a href={`tel:${contact.phone}`} target="_blank" rel="noreferrer">
+            <FiPhoneCall className="social__icon_contact" />
+          </a>
         </div>
       </div>
     </div>
