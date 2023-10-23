4bd92f8e0388272f384aae98da20fa01dbaf15dc
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-10-23 11:15:13 +0530

    css

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Leaderboard.css";
-import Aos from "aos";
 import titleVectorLeft from "../../assets/svg/title_vector_left.svg";
 import titleVecotrRight from "../../assets/svg/title__vector_right.svg";
-import LeaderTable from "./LeaderTable";
+// import LeaderTable from "./LeaderTable";
 import { FiArrowLeft } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import { useScreenWidth } from "../../hooks/useScreenWidth";
+import Lottie from "lottie-react";
+import comingSoonAnimation from "../../assets/animations/coming_soon.json";
 const Leaderboard = () => {
-  const navigate = useNavigate();
+  const size = useScreenWidth();
+  const [animationSize, setAnimationSize] = useState();
   useEffect(() => {
-    Aos.init({ duration: 700 });
-  }, []);
+    if (size > 600) {
+      setAnimationSize(450);
+    } else if (size > 500) {
+      setAnimationSize(450);
+    } else if (size > 400) {
+      setAnimationSize(400);
+    } else {
+      setAnimationSize(300);
+    }
+  }, [size]);
+  const navigate = useNavigate();
   const goBack = () => {
     navigate("/");
   };
@@ -22,7 +34,17 @@ const Leaderboard = () => {
         <h1>LEADERBOARD</h1>
         <img src={titleVecotrRight} alt="" />
       </div>
-      <LeaderTable />
+      <div className="coming__son">
+        <Lottie
+          animationData={comingSoonAnimation}
+          loop={true}
+          autoplay={true}
+          style={{ width: animationSize, height: animationSize }}
+        />
+      </div>
+      
+
+      {/* <LeaderTable /> */}
     </div>
   );
 };
