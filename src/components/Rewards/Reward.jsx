d013682f5eacdf1834f3e09b5a72fe87b16ff210
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-10-25 17:55:28 +0530

    css

--- src/components/Rewards/Reward.jsx
+++ src/components/Rewards/Reward.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Reward.css";
-import firstplace from "../../assets/svg/first place medal_.svg";
-import secondplace from "../../assets/svg/second place medal_.svg";
-import thirdplace from "../../assets/svg/third place medal_.svg";
+// import firstplace from "../../assets/svg/first place medal_.svg";
+// import secondplace from "../../assets/svg/second place medal_.svg";
+// import thirdplace from "../../assets/svg/third place medal_.svg";
 import titleVectorLeft from "../../assets/svg/title_vector_left.svg";
 import titleVecotrRight from "../../assets/svg/title__vector_right.svg";
 import reward_section_img from "../../assets/svg/reward_section_img.svg";
@@ -39,39 +39,13 @@ function Reward() {
                 aos-delay="200"
                 className="reward__cards__priz reward__first__prize"
               >
-                <img
-                  src={firstplace}
-                  alt=""
-                  style={{ pointerEvents: "none" }}
-                />
-                <h2>10,000</h2>
+                
+                <h2 style={{textAlign:'center'}}>Exciting rewards worth 15k !</h2>
               </div>
-              <div
-                data-aos="zoom-in"
-                aos-delay="350"
-                className="reward__cards__priz reward__second__prize"
-              >
-                <img
-                  src={secondplace}
-                  alt=""
-                  style={{ pointerEvents: "none" }}
-                />
-                <h2>10,000</h2>
-              </div>
+              
             </div>
             <div className="reward__right__middle">
-              <div
-                data-aos="zoom-in"
-                aos-delay="450"
-                className="reward__cards__priz reward__third__prize"
-              >
-                <img
-                  src={thirdplace}
-                  alt=""
-                  style={{ pointerEvents: "none" }}
-                />
-                <h2>10,000</h2>
-              </div>
+              
             </div>
             <div className="reward__ul__item">
               <ul
