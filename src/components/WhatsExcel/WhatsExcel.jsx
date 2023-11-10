fb8ed057be0bf73726ca56c3b084757b1b8c7958
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-11-10 19:45:10 +0530

    css and mascot

--- src/components/WhatsExcel/WhatsExcel.jsx
+++ src/components/WhatsExcel/WhatsExcel.jsx
@@ -3,6 +3,7 @@ import titleVectorLeft from "../../assets/svg/title_vector_left.svg";
 import titleVecotrRight from "../../assets/svg/title__vector_right.svg";
 import "./WhatsExcel.css";
 import ImageSlider from "./WhatExcelCard";
+import juno from '../../assets/png/junoMasot.png'
 
 function WhatsExcel() {
   return (
@@ -12,13 +13,18 @@ function WhatsExcel() {
         <h1>WHAT IS EXCEL</h1>
         <img src={titleVecotrRight} alt="" />
       </div>
-      <p data-aos="fade-up" aos-delay="400" className="whats_excel_content">
-        Excel is India’s second largest techno-managerial festival hosted by
-        Govt. Model Engineering College, Kochi. Over the years, more than 2000
-        universities and 1000 institutions from around the country have been
-        part of Excel. With the rich legacy of 23 successful editions of Excel,
-        be geared up for an even more exhilarating event this Excel 2023!
-      </p>
+      <div className="mascot__about_container">
+        <img src={juno} alt="excel2023mascot" />
+        <p data-aos="fade-up" aos-delay="400" className="whats_excel_content">
+          Excel is India’s second largest techno-managerial festival hosted by
+          Govt. Model Engineering College, Kochi. Over the years, more than 2000
+          universities and 1000 institutions from around the country have been
+          part of Excel. With the rich legacy of 23 successful editions of
+          Excel, be geared up for an even more exhilarating event this Excel
+          2023!
+        </p>
+      </div>
+
       <ImageSlider />
     </div>
   );
